3eeeb05f7b3356a6d12761f32b8469f6667b2339
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-12-16 03:12:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,100 @@
+// Question no : 1
+//   let num = prompt("Enter a anyone number");
+
+//   if (num >= 48 && num <= 57){
+//      document.write("this is a number")
+//    }  
+//     else if (num >= 65 && num <= 90){
+//      document.write("Upper case")
+//    } 
+//     else if (num >= 97 && num <= 122){
+//      document.write("Lower case")
+//    } 
+//     else{
+//      document.write("Enter a valid number")
+// };
+
+
+// Question no : 2
+// let num1 = prompt("Enter a number.");
+// let num2 = prompt("Enter a number.");
+
+//     if (num1 > num2){
+//      document.write("Large num")
+//     }
+//     else if (num1 < num2){
+//       document.write("Small num")
+//     }
+//     else if(num1 === num2){
+//       document.write("both are equal num")
+//     };
+
+    
+// Question no : 3
+    // let input = prompt("Enter a number:");
+    
+    // if (input > 1){
+    //   document.write("Positive number");
+    // }
+    //  else if(input < 1){
+    //   document.write("Negative number");
+    // }   
+    // else {
+    //   document.write("its a zero (0)");
+    // }
+
+
+// Question no : 4
+//    let letter = prompt("Enter a vowel letters :- \n Capital Or Small :");
+    
+//     if (letter == "a" || letter == "A" || letter == "e"|| letter == "E"|| letter == "i" || letter == "I" || letter == "o" || letter == "O" || letter == "u" || letter == "U"){
+//       document.write("True");
+//     }
+//     else {
+//       document.write("False");
+//     }
+
+
+// Question no : 5
+//    let password = 'password@123';
+//    let user = prompt("Enter the password:");
+    
+//     if (user === "" ){
+//       document.write("please enter the password:");
+//     }
+//     else if(user == password){
+//       document.write("Correct! The password you entered matches the original password..!!")
+//     }
+//     else if(user != password){
+//       document.write("Incorrect password..")
+//     }
+
+
+// Question no : 6
+    // let greeting;
+    // let hour = 13;
+    
+    // if (hour < 18){
+    //    greeting = "Good day";
+    // }
+    // else{
+    //    greeting = "Good evening";
+    // }
+
+
+// Question no : 7
+    // let time = 1900;
+    
+    // if (time >= 0.00 && time < 1700 ){
+    //   document.write("Good Morning");
+    // }
+    // else if (time >= 1200 && time < 1700){
+    //   document.write("Good Afternoor");
+    // }
+    // else if (time >= 1700 && time < 2100){
+    //   document.write("Good Evening");
+    // }
+    // else if (time >=2100 && time < 2359){
+    //     document.write("Good Night");
+    // }
+        
